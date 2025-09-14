38b46092a3ebba514c885ea4c68099910d905d00
Oyetomi <oyetomiabiodun@yahoo.com>
2025-09-14 23:05:15 +0100

    abstract data types

--- designing-algorithms/towersOfHanoiFormula.js
+++ designing-algorithms/towersOfHanoiFormula.js
@@ -1,3 +1,8 @@
+/**
+ *@property helper pegs numbered 1,2,3 then 1+2+3 = 6
+ * so if start = 1 and end = 3 then the helper = 6(1+3) = 2
+ * This automatically calculates which peg to use as the helper
+ */
 const hanoi = (n, start, end) => {
   if (n === 1) {
     console.log(`Move disk from ${start} to ${end}`)
